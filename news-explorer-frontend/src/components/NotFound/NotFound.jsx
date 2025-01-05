8c116f07d96967d90e06f21bcab2809b358832ac
RicardoPerdomo <ricardoperdomo0814@gmail.com>
2025-01-05 10:43:42 -0500

    fixes

--- news-explorer-frontend/src/components/NotFound/NotFound.jsx
+++ news-explorer-frontend/src/components/NotFound/NotFound.jsx
@@ -1,12 +1,17 @@
 import "./NotFound.css";
+import NotFoundIcon from "../../assets/not-found.svg";
 
 function NotFound() {
   return (
     <div className="not-found">
-      <img className="not-found__icon" alt="not found icon" />
+      <img
+        className="not-found__icon"
+        src={notFoundIcon}
+        alt="not found icon"
+      />
       <h2 className="not-found__title">Nothing found</h2>
       <p className="not-found__text">
-        Sorry, but nothing matched your seach terms.
+        Sorry, but nothing matched your search terms.
       </p>
     </div>
   );
